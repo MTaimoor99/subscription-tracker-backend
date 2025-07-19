a59d6acc7cfc058ce3ef7af2acc38af0f5709749
Muhammad Taimoor <taimoor992002@gmail.com>
2025-07-19 16:33:15 +0500

    Successfully read response in the backend sent using Flutter frontend.

--- src/main/java/com/example/subscription_tracker_backend/auth/Authentication.java
+++ src/main/java/com/example/subscription_tracker_backend/auth/Authentication.java
@@ -1,6 +1,8 @@
 package com.example.subscription_tracker_backend.auth;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -8,9 +10,10 @@ import java.util.Map;
 
 @RestController
 public class Authentication {
-    @GetMapping("/register")
-    public Map<String,Object> registration(String email, String password){
+    @PostMapping("/auth/register")
+    public Map<String,Object> registration(@RequestBody Map<String,String> body){
         Map<String,Object> response = new HashMap<>();
+
         return response;
     }
 }
