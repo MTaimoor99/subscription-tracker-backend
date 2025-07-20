eddf8923d2c0701bd385dc442151ec5a442d7a50
Muhammad Taimoor <taimoor992002@gmail.com>
2025-07-20 16:08:42 +0500

    connected app to a spring boot backend

--- src/main/java/com/example/subscription_tracker_backend/auth/Authentication.java
+++ src/main/java/com/example/subscription_tracker_backend/auth/Authentication.java
@@ -1,19 +1,13 @@
 package com.example.subscription_tracker_backend.auth;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class Authentication {
     @PostMapping("/auth/register")
-    public Map<String,Object> registration(@RequestBody Map<String,String> body){
-        Map<String,Object> response = new HashMap<>();
-
-        return response;
+    public Map<String,Object> registration(@RequestBody Map<String,Object> body){
+        return body;
     }
 }
